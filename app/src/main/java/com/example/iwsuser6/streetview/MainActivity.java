1cb4be02414d0fe176df4846f45cb520f342efc4
raviteja <malipeddiraviteja@gmail.com>
2016-08-10 12:41:44 +0530

    Add some code in MainActitivity

--- app/src/main/java/com/example/iwsuser6/streetview/MainActivity.java
+++ app/src/main/java/com/example/iwsuser6/streetview/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Add some code
 
         SupportStreetViewPanoramaFragment supportStreetViewPanoramaFragment = (SupportStreetViewPanoramaFragment) getSupportFragmentManager().findFragmentById(R.id.streetview);
 
